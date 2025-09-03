5b316b709e33e38a8adb1fff193de1d7da2309fc
Austin Merrill <austinm9506@gmail.com>
2025-09-03 00:55:21 -0400

    Added createToDoHandler for POST requests

--- pkg/api/handler.go
+++ pkg/api/handler.go
@@ -6,9 +6,11 @@ import (
 	"encoding/json"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/AJMerr/MSE/pkg/store"
+	"github.com/AJMerr/NYMToDo/pkg/todo"
 	"github.com/AJMerr/gonk/pkg/router"
 )
 
@@ -16,10 +18,12 @@ type Handler struct {
 	S *store.Store
 }
 
-func New(s *store.Store) *Handler
+func New(s *store.Store) *Handler {
+	return &Handler{S: s}
+}
 
 func (h *Handler) RegisterRouter(r *router.Router) {
-	r.GET("/todos", h.createToDoHandler)
+	r.POST("/todos", h.createToDoHandler)
 }
 
 // Helper functions
@@ -57,5 +61,58 @@ func writeIndex(s *store.Store, ids []string) error {
 }
 
 func (h *Handler) createToDoHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
 
+	var req struct {
+		Title       string `json:"title"`
+		Description string `json:"description"`
+	}
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	if err := dec.Decode(&req); err != nil {
+		http.Error(w, "invalid JSON", http.StatusBadRequest)
+		return
+	}
+	title := strings.TrimSpace(req.Title)
+	if title == "" {
+		http.Error(w, "Title cannot be empty", http.StatusBadRequest)
+		return
+	}
+	description := strings.TrimSpace(req.Description)
+
+	id := newID()
+	t := todo.ToDo{
+		ID:          id,
+		Title:       title,
+		Description: description,
+		IsCompleted: false,
+	}
+
+	tb, err := json.Marshal(t)
+	if err != nil {
+		http.Error(w, "store_error", http.StatusInternalServerError)
+		return
+	}
+	if err := h.S.Set(makeKey(id), tb); err != nil {
+		http.Error(w, "store_error", http.StatusInternalServerError)
+		return
+	}
+
+	ids, err := readIndex(h.S)
+	if err != nil {
+		http.Error(w, "read_index_error", http.StatusInternalServerError)
+		return
+	}
+	ids = append(ids, id)
+	if err := writeIndex(h.S, ids); err != nil {
+		http.Error(w, "write_index_error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	_ = json.NewEncoder(w).Encode(t)
 }
