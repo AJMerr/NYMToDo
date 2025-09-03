48c90d5cd081d4594630777611ffa2912bdb6ef8
Austin Merrill <austinm9506@gmail.com>
2025-09-02 23:42:32 -0400

    Removed the interface and added a completed to the ToDo struct

--- pkg/todo/model.go
+++ pkg/todo/model.go
@@ -4,11 +4,5 @@ type ToDo struct {
 	ID          string
 	Title       string
 	Description string
-}
-
-type KV interface {
-	Set(key string, val []byte) error
-	Get(key string) ([]byte, bool)
-	Del(key string) bool
-	Exists(key string) bool
+	Completed   bool
 }
